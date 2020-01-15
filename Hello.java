a87c13a5e41335f62eba574a0f7d2c4d946a613d
yupadhyay23 <59926863+yupadhyay23@users.noreply.github.com>
2020-01-15 10:38:27 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,28 @@
+/*********************************************
+* Hello.java
+**********************************************
+* Yash Upadhyay
+* January 15, 2020
+* CMSC 255, Section 003 */
+
+
+
+
+public class Hello {
+    public static void main(String[] args) {
+
+       System.out.println("Yash Upadhyay");
+       System.out.println("Hello.java");
+       System.out.println("This project outputs name, java project name, project description, version date, course number and section, in this order.");
+       System.out.println("January 15, 2020");
+       System.out.println("CMSC 255, Section 003");
+       System.out.println();
+   }
+
+      }
+
+
+
+
+
+
